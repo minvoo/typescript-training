641e85f96f51885a7e9472c0e0e2691f18faad76
minvoo <maciejewski_mariusz@wp.pl>
2023-01-07 10:45:14 +0100

    growable arrays

---
+++ 03-loops-and-arrays/growable-arrays.js
@@ -0,0 +1,6 @@
+var arr = [1, 2, 3, 4];
+arr.push(5);
+for (var _i = 0, arr_1 = arr; _i < arr_1.length; _i++) {
+    var num = arr_1[_i];
+    console.log(num);
+}
---
+++ 03-loops-and-arrays/growable-arrays.ts
@@ -0,0 +1,7 @@
+let arr = [1,2,3,4];
+
+arr.push(5);
+
+for (let num of arr) {
+console.log(num);
+}
